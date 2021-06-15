4013f73ec5a1595e766e7ccc133ec4dbb1633797
Reinold Rivera <oknolaz.freedom@protonmail.com>
2021-06-15 17:13:30 +0300

    v0.7.0

--- skills/show_about.py
+++
@@ -1,8 +0,0 @@
-from ui import about_window
-from PyQt5 import QtWidgets
-
-
-class ShowAboutWindow(QtWidgets.QWidget, about_window.Ui_Dialog):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
